5ca03677737c0ee1f8bb3a407c0c701d065d0bcd
abhishek36 <akamble9913@gmail.com>
2022-01-17 13:53:48 +0530

    first commit

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,35 @@
+//import Book model here
+
+const addBook = async (req, res) => {
+  //addBook api logic here
+};
+
+const getAllBook = async (req, res) => {
+  //getAllBook api logic here
+};
+
+const getBookByCategoryId = async (req, res) => {
+  //getBookByCategoryId api logic here
+};
+
+const getBookById = async (req, res) => {
+  //getBookById api logic here
+};
+
+const updateBookById = async (req, res) => {
+  //updateBookById api logic here
+};
+
+const deleteBookById = async (req, res) => {
+  //deleteBookById api logic here
+};
+
+const BookController = {
+  addBook,
+  getAllBook,
+  getBookByCategoryId,
+  getBookById,
+  updateBookById,
+  deleteBookById,
+};
+module.exports = BookController;
---
+++ src/controllers/category.controller.js
@@ -0,0 +1,31 @@
+//import Category model here
+
+const addCategory = async (req, res) => {
+  //addCategory api logic here
+};
+
+const getAllCategory = async (req, res) => {
+  //getAllCategory api logic here
+};
+
+const getCategoryById = async (req, res) => {
+  //getCategoryById api logic here
+};
+
+const updateCategoryById = async (req, res) => {
+  //updateCategoryById api logic here
+};
+
+const deleteCategoryById = async (req, res) => {
+  //deleteCategoryById api logic here
+};
+
+const CategoryController = {
+  addCategory,
+  search,
+  getAllCategory,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+};
+module.exports = CategoryController;
